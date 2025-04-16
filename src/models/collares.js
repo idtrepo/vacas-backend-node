81c3612df2efbd938cd7ebc85a8e9555bbb96024
Luis Mario Torres Moreno <luisillo77mario@gmail.com>
2025-04-16 10:43:47 -0700

    fix(models): arreglado error de implementacion del modelo

--- src/models/collares.js
+++ src/models/collares.js
@@ -4,8 +4,8 @@ import { collaresSchemaLeer } from '../schemas/collares.js';
 export class CollarModel {
     static async obtenerElementos({ skip, take, where }) {
         try {
-            const numElementos = await prisma.collares.count({ where });
-            const elementos = await prisma.collares.findMany({
+            const numElementos = await prisma.collar.count({ where });
+            const elementos = await prisma.collar.findMany({
                 skip, take, where, orderBy: [{ creado: 'desc' }], select: collaresSchemaLeer
             });
             return { numElementos, elementos };
@@ -16,7 +16,7 @@ export class CollarModel {
 
     static async crearElemento({ data }) {
         try {
-            const elemento = await prisma.collares.create({ data, select: collaresSchemaLeer });
+            const elemento = await prisma.collar.create({ data, select: collaresSchemaLeer });
             return elemento;
         } catch (err) {
             throw err;
@@ -25,7 +25,7 @@ export class CollarModel {
 
     static async obtenerElemento({ id }) {
         try {
-            const elemento = await prisma.collares.findFirstOrThrow({ where:{id}, select: collaresSchemaLeer });
+            const elemento = await prisma.collar.findFirstOrThrow({ where:{id}, select: collaresSchemaLeer });
             return elemento;
         } catch (err) {
             throw err;
@@ -34,7 +34,7 @@ export class CollarModel {
 
     static async editarElemento({ id, data }) {
         try {
-            const elemento = await prisma.collares.update({
+            const elemento = await prisma.collar.update({
                 where:{id},
                 data,
                 select: collaresSchemaLeer
